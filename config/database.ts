3b2cdadb3f6ea236c6e7a113024c3353ef7558b6
mridx <mridulbaishya28@gmail.com>
2021-07-21 22:59:02 +0530

    changed pg config

--- config/database.ts
+++ config/database.ts
@@ -68,7 +68,10 @@ const databaseConfig: DatabaseConfig = {
         user: Env.get('PG_USER'),
         password: Env.get('PG_PASSWORD', ''),
         database: Env.get('PG_DB_NAME'),
-        ssl: true,
+        ssl: {
+          rejectUnauthorized: false,
+        }
+
       },
       migrations: {
         naturalSort: true,
